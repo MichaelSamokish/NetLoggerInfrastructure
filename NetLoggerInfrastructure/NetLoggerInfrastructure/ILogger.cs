3bf8770396d6d190d111322610ed3aea1d87c929
MichaelSamokish <samich2006@gmail.com>
2015-12-11 14:08:20 +0300

    Changed folder structure

--- NetLoggerInfrastructure/NetLoggerInfrastructure/ILogger.cs
+++ NetLoggerInfrastructure/NetLoggerInfrastructure/ILogger.cs
@@ -6,6 +6,10 @@ using System.Threading.Tasks;
 
 namespace NetLoggerInfrastructure
 {
+
+    /// <summary>
+    /// Уровни логгирования
+    /// </summary>
     public enum LoggerLevel
     {
         Fatal = 0,
